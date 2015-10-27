0bed85dc017e38651c1c2f26bf93b0271326b9dd
SIPU <ddasik00@naver.com>
2015-10-27 16:56:09 +0900

    Update SipuInteractive.js

--- SipuInteractive.js
+++ SipuInteractive.js
@@ -46,17 +46,17 @@ var SIPU = (function(SIPU, $, undefined) {
 		return imgs;
 	}
 
-	function do_event (ran, can, iamgelist) {
+	function do_input_event (ran, can, iamgelist) {
 		$.each(ran,function (index) {
 			$(ran[index].node).on("change mousemove input", function () {
 				$(this).attr("min","1");
 				$(this).attr("max","255");
-				canvas_event(can[index].node, iamgelist[index], $(this).val());
+				do_canvas_event(can[index].node, iamgelist[index], $(this).val());
 			});
 		});
 	}
 
-	function canvas_event (can, img, val) {
+	function do_canvas_event (can, img, val) {
 		var ctx = can.getContext("2d");
 		var sq = Math.ceil( val / (255 / (img.length - 1)));
 		var sqfl = Math.ceil(val / (255 / (img.length - 1))) - (val / (255 / (img.length - 1)));
@@ -67,12 +67,25 @@ var SIPU = (function(SIPU, $, undefined) {
 		ctx.globalAlpha = sqfl;
 	}
 
+	function do_play_button(ran, can, img) {
+		$.each(ran,function (index) {
+			var i = $(ran[index].node).val();
+			var max = $(ran[index].node).attr("max");
+			var timerId = setInterval(function () {
+				ran[index].node.value = i;
+				do_canvas_event(can[index].node, img[index], i);
+				if (i === max) { clearInterval(timerId); }
+				i++;
+			} ,30);
+		});
+	}
+
 	SIPU.run = function () {
 		var image_list = getimagelist(getSelector("timelapse","DL"));
 		var input_range_list = getSelector("timelapse","INPUT");
 		var canvas_list = getSelector("timelapse","CANVAS");
 
-		do_event(input_range_list, canvas_list, image_list);
+		do_input_event(input_range_list, canvas_list, image_list);
 	};
 	return SIPU;
 })(window.SIPU || {}, jQuery);
